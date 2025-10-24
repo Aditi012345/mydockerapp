7de25524556f652b9f112127f7c19ac2149b0b1e
Aditi Deshpande <aditideshpande3004@gmail.com>
2025-10-24 19:47:04 +0530

    Initial commit with Docker and Jenkins files

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Dockerized Java App!");
+        System.out.println("Deployed successfully via Jenkins Pipeline!");
+    }
+}
